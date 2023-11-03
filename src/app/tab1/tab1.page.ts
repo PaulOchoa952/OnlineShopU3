01d8791f19edfa7b7c25847c7854730ef8cd4a6c
paul <paalochoame@ittepic.edu.mx>
2023-11-03 09:44:18 -0700

    Actualizar un producto

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,123 @@
+import { Component } from '@angular/core';
+import { Product } from '../models/product.model';
+import { CartService } from '../services/cart.service';
+import { ProductService } from '../services/product.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  public products: Product[] = [];
+  public productsFounds: Product[] = [];
+  public filter = [
+    "Abarrotes",
+    "Frutas y Verduras",
+    "Limpieza",
+    "Farmacia",
+  ];
+
+  public colors = [
+    {
+      type: "Abarrotes",
+      color: "primary"
+    },
+    {
+      type: "Frutas y Verduras",
+      color: "secondary"
+    },
+    {
+      type: "Limpieza",
+      color: "warning"
+    },
+    {
+      type: "Farmacia",
+      color: "danger"
+    }
+  ];
+//se importo el servicio en un constructor
+  constructor(private cartService: CartService,private router:Router,
+    private productService: ProductService) {
+    this.products.push({
+      name: "Aguacate",
+      price: 100,
+      description: "Lorem ipsum dolor sit amet.",
+      type: "Frutas y Verduras",
+      photo: "https://picsum.photos/500/300?random",
+    });
+    this.products.push({
+      name: "Coca Cola",
+      price: 20,
+      description: "Lorem ipsum dolor sit amet.",
+      type: "Abarrotes",
+      photo: "https://picsum.photos/500/300?random"
+    });
+    this.products.push({
+      name: "Jabón Zote",
+      price: 40,
+      description: "Lorem ipsum dolor sit amet.",
+      type: "Limpieza",
+      photo: "https://picsum.photos/500/300?random"
+    });
+    this.products.push({
+      name: "Aspirina",
+      price: 50,
+      description: "Lorem ipsum dolor sit amet.",
+      type: "Farmacia",
+      photo: "https://picsum.photos/500/300?random"
+    });
+    this.productsFounds = this.products;
+
+  }
+//se puso el servicio en el ionViewWillEnter
+  ionViewWillEnter() {
+    this.products=this.productService.getProducts();
+    this.productsFounds = this.products;
+  }
+
+  public getColor(type: string): string {
+    const itemFound = this.colors.find((element) => {
+      return element.type === type;
+    });
+    let color = itemFound && itemFound.color ? itemFound.color : "";
+    return color;
+  }
+
+  public filterProducts(): void {
+    console.log(this.filter);
+    this.productsFounds = this.products.filter(
+      item => {
+        return this.filter.includes(item.type);
+      }
+    );
+  }
+
+  public addToCart(product: Product, i: number) {
+    product.photo = product.photo + i;
+    this.cartService.addToCart(product);
+    console.log(this.cartService.getCart());
+  }
+  public openAddProductPage(){
+    this.router.navigate(['/add-product']);
+  }
+public deleteProduct(product:Product,i:number){
+  this.productService.removeProduct(i);
+  console.log(this.productService.getProducts());
+}
+public openUpdateProduct(productToEdit: Product, position: number) {
+  this.router.navigate(['/update-product', {
+    productToEdit: JSON.stringify(productToEdit),
+    position: position
+  }]);
+}
+
+
+
+
+
+
+
+}
